f302ff2a6caf6fd70d77a7a6896f61d59c47c1cc
Beatriz <btovar0002@mymail.lausd.net>
2026-01-29 10:48:24 -0800

    Initial Commit

---
+++ Mathtutor/MathtutorApp.swift
@@ -0,0 +1,17 @@
+//
+//  MathtutorApp.swift
+//  Mathtutor
+//
+//  Created by TOVAR, BEATRIZ on 1/29/26.
+//
+
+import SwiftUI
+
+@main
+struct MathtutorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
